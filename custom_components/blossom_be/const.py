a03ff46f0e643f037005009b7f5ffc3da79c4aeb
thomas-svrts <81048302+thomas-svrts@users.noreply.github.com>
2024-12-08 16:12:23 +0100

    Update const.py

--- custom_components/blossom_be/const.py
+++ custom_components/blossom_be/const.py
@@ -7,13 +7,4 @@ DATA_URL = "https://api.blossom.be/"
 ICON = "mdi:currency-eur"
 COMPONENT_TITLE = "Blossom be"
 
-CONF_COORDINATOR = "coordinator"
-ATTR_TIME = "from_time"
-
-DATA_ELECTRICITY = "electricity"
-DATA_GAS = "gas"
-DATA_MONTH_SUMMARY = "month_summary"
-DATA_INVOICES = "invoices"
-
-SERVICE_NAME_PRICES = "Prices"
-SERVICE_NAME_COSTS = "Costs"
+CONF_REFRESH_TOKEN = "refresh_token"
