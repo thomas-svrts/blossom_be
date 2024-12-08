cd2bf068cea8e6d7ddabea9dd681a635550a94c2
thomas-svrts <81048302+thomas-svrts@users.noreply.github.com>
2024-12-08 13:14:18 +0100

    Add files via upload

---
+++ custom_components/detailed_hello_world_push/__init__.py
@@ -0,0 +1,37 @@
+"""The Detailed Hello World Push integration."""
+
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
+
+from . import hub
+
+# List of platforms to support. There should be a matching .py file for each,
+# eg <cover.py> and <sensor.py>
+PLATFORMS = [Platform.SENSOR, Platform.COVER]
+
+type HubConfigEntry = ConfigEntry[hub.Hub]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: HubConfigEntry) -> bool:
+    """Set up Hello World from a config entry."""
+    # Store an instance of the "connecting" class that does the work of speaking
+    # with your actual devices.
+    entry.runtime_data = hub.Hub(hass, entry.data["host"])
+
+    # This creates each HA object for each platform your device requires.
+    # It's done by calling the `async_setup_entry` function in each platform module.
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    # This is called when an entry/configured device is to be removed. The class
+    # needs to unload itself, and remove callbacks. See the classes for further
+    # details
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+    return unload_ok
