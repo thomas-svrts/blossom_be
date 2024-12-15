7c8f0cfc5a12757bb64d225b0ac10aa33c16a217
thomas-svrts <81048302+thomas-svrts@users.noreply.github.com>
2024-12-15 21:06:39 +0100

    Update sensor.py

--- custom_components/blossom_be/sensor.py
+++ custom_components/blossom_be/sensor.py
@@ -95,10 +95,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
         BlossomSensor(coordinator, "min_charge_rate", device_id,        "set_points.min_charge_rate",         SensorDeviceClass.POWER, None, UnitOfPower.WATT, EntityCategory.DIAGNOSTIC ),
         BlossomSensor(coordinator, "current_month_peak", device_id,     "set_points.current_month_peak",      SensorDeviceClass.POWER, SensorStateClass.MEASUREMENT, UnitOfPower.WATT, None ),   
         BlossomSensor(coordinator, "monthly_energy_consumption", device_id, "consumption.carConsumptionWh",    SensorDeviceClass.ENERGY, SensorStateClass.TOTAL_INCREASING, UnitOfEnergy.WATT_HOUR, None ),   
-        BlossomSensor(coordinator, "session_status", device_id, "home-charging-session.session.status",    None, None, None, None ),   
-        BlossomSensor(coordinator, "session_consumption", device_id, "home-charging-session.session.kWh",    SensorDeviceClass.ENERGY, SensorStateClass.MEASUREMENT, UnitOfEnergy.KILO_WATT_HOUR, None ),   
-        BlossomSensor(coordinator, "session_start", device_id, "home-charging-session.session.time_started_session",    SensorDeviceClass.TIMESTAMP, None, None, None ),
-        BlossomSensor(coordinator, "home_charging_status", device_id, "home-charging-session.status",    None, None, None, None ),   
+        BlossomSensor(coordinator, "session_status", device_id,         "home-charging-session.session.status",    None, None, None, None ),   
+        BlossomSensor(coordinator, "session_consumption", device_id,    "home-charging-session.session.kWh",    SensorDeviceClass.ENERGY, SensorStateClass.MEASUREMENT, UnitOfEnergy.KILO_WATT_HOUR, None ),   
+        BlossomSensor(coordinator, "session_start", device_id,          "home-charging-session.session.time_started_session",    SensorDeviceClass.TIMESTAMP, None, None, None ),
+        BlossomSensor(coordinator, "home_charging_status", device_id,   "home-charging-session.status",    None, None, None, None ),   
     ]
     
     async_add_entities(entities)
